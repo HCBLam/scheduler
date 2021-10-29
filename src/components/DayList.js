26791407546f4ad063fff81f052728f824c4c70e
HL <HL@HLs-MacBook-Pro.local>
2021-10-29 12:51:06 -0400

    Add DayList component

---
+++ src/components/DayList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import DayListItem from "./DayListItem";
+
+export default function DayList(props) {
+  const {days} = props;
+
+  const parsedDays = days.map(day => <DayListItem key={day.id} {...day} selected={day.name === props.value} setDay={props.onChange}/>);
+
+  return (
+    <ul>
+      {parsedDays}
+    </ul>
+  )
+}
